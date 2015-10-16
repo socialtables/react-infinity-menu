22d13567072f57469b9572b748e13f9bb95352c2
Sijie Tian <sijie@socialtables.com>
2015-10-16 12:54:42 -0400

    added more functional test

--- test/index.js
+++ test/index.js
@@ -1,68 +1,147 @@
-var React = require("react/addons");
-var TestUtils = React.addons.TestUtils;
-var shallowRenderer = TestUtils.createRenderer();
-var ObjectsOptions = require("../dist/objects-options.js");
-var assert = require('assert');
+import React from "react/addons";
+let TestUtils = React.addons.TestUtils;
+let shallowRenderer = TestUtils.createRenderer();
+import ObjectsOptions from "../dist/objects-options";
+import assert from "assert";
+import sinon from "sinon";
+import "should-sinon";
 
 describe("Test for ObjectsOptions", function() {
-	it('should render correctly', function () {
-		var component = React.createElement(ObjectsOptions);
+
+	let component;
+	let dom;
+	let onNodeMouseClickStub, onLeafMouseClickStub, onLeafMouseDownStub, onLeafMouseUpStub, onCloseStub;
+
+	before(function() {
+		const tree =
+		[
+			{
+				name: "menu1",
+				id: 1,
+				isOpen: true,
+				children: [
+					{
+						name: "submenu1",
+						id: 1,
+						isOpen: true,
+						children: [
+							{
+								name: "item1-1",
+								id: 1
+							},
+							{
+								name: "item1-2",
+								id: 2
+							}
+						]
+					},
+					{
+						name: "submenu2",
+						id: 2,
+						isOpen: true,
+						children: [
+							{
+								name: "item2-1",
+								id: 1
+							}
+						]
+					}
+				]
+			},
+			{
+				name: "menu2",
+				id: 2,
+				isOpen: true,
+				children: [
+					{
+						name: "item3-1",
+						id: 1
+					}
+				]
+			}
+		];
+		onNodeMouseClickStub = sinon.stub();
+		onLeafMouseClickStub = sinon.stub();
+		onLeafMouseUpStub = sinon.stub();
+		onLeafMouseDownStub = sinon.stub();
+		onCloseStub = sinon.stub();
+		component = <ObjectsOptions
+			tree={tree}
+			onNodeMouseClick={onNodeMouseClickStub}
+			onLeafMouseClick={onLeafMouseClickStub}
+			onLeafMouseUp={onLeafMouseUpStub}
+			onLeafMouseDown={onLeafMouseDownStub}
+			onClose={onCloseStub}
+			/>;
+		dom = TestUtils.renderIntoDocument(component);
+	});
+
+	it("should render correctly with folder tree", function () {
 		shallowRenderer.render(component);
 		var result = shallowRenderer.getRenderOutput();
 		assert.equal(result.props.className, "st-vm-objects-options");
 	});
 
-	it('should render correctly with folder tree', function () {
-		var component = React.createElement(ObjectsOptions, {tree:
-			[
-				{
-					name: "menu1",
-					id: 1,
-					isOpen: true,
-					children: [
-						{
-							name: "submenu1",
-							id: 1,
-							isOpen: true,
-							children: [
-								{
-									name: "item1-1",
-									id: 1
-								},
-								{
-									name: "item1-2",
-									id: 2
-								}
-							]
-						},
-						{
-							name: "submenu2",
-							id: 2,
-							isOpen: true,
-							children: [
-								{
-									name: "item2-1",
-									id: 1
-								}
-							]
-						}
-					]
-				},
-				{
-					name: "menu2",
-					id: 2,
-					isOpen: true,
-					children: [
-						{
-							name: "item3-1",
-							id: 1
-						}
-					]
-				}
-			]
-		});
-		shallowRenderer.render(component);
-		var result = shallowRenderer.getRenderOutput();
-		assert.equal(result.props.className, "st-vm-objects-options");
+
+	it("should call onNodeMouseClick function", function () {
+		var folderNode = React.findDOMNode(
+			TestUtils.scryRenderedDOMComponentsWithClass(
+				dom,
+				"st-vm-objects-options-folder"
+			)[0]
+		);
+		onNodeMouseClickStub.should.have.callCount(0);
+		TestUtils.Simulate.click(folderNode);
+		onNodeMouseClickStub.should.have.callCount(1);
 	});
+
+	it("should call onLeafMouseClick function", function () {
+		var leaf = React.findDOMNode(
+			TestUtils.scryRenderedDOMComponentsWithClass(
+				dom,
+				"st-vm-objects-options-folder-item"
+			)[0]
+		);
+		onLeafMouseClickStub.should.have.callCount(0);
+		TestUtils.Simulate.click(leaf);
+		onLeafMouseClickStub.should.have.callCount(1);
+	});
+
+	it("should call onLeafMouseDown function", function () {
+		var leaf = React.findDOMNode(
+			TestUtils.scryRenderedDOMComponentsWithClass(
+				dom,
+				"st-vm-objects-options-folder-item"
+			)[0]
+		);
+		onLeafMouseDownStub.should.have.callCount(0);
+		TestUtils.Simulate.mouseDown(leaf);
+		onLeafMouseDownStub.should.have.callCount(1);
+	});
+
+	it("should call onLeafMouseUp function", function () {
+		var leaf = React.findDOMNode(
+			TestUtils.scryRenderedDOMComponentsWithClass(
+				dom,
+				"st-vm-objects-options-folder-item"
+			)[0]
+		);
+		onLeafMouseUpStub.should.have.callCount(0);
+		TestUtils.Simulate.mouseUp(leaf);
+		onLeafMouseUpStub.should.have.callCount(1);
+	});
+
+
+	it("should call onClose function", function () {
+		var closeButton = React.findDOMNode(
+			TestUtils.findRenderedDOMComponentWithClass(
+				dom,
+				"st-icon-arrow-left"
+			)
+		);
+		onCloseStub.should.have.callCount(0);
+		TestUtils.Simulate.click(closeButton);
+		onCloseStub.should.have.callCount(1);
+	});
+
 });
